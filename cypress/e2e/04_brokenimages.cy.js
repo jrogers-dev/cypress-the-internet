b853242ead54f117a4a706d0e33749f685715569
Mimetic <jrogers.dev@gmail.com>
2023-03-01 18:27:33 -0800

    Added spec file for Broken_Images page

---
+++ cypress/e2e/04_brokenimages.cy.js
@@ -0,0 +1,29 @@
+describe('Completes tests on elements at https://the-internet.herokuapp.com/broken_images', () => {
+  it('Passes if the page exists', () => {
+    cy.visit('https://the-internet.herokuapp.com/broken_images');
+  });
+
+  it('Passes if the first image is working', () => {
+    cy.visit('https://the-internet.herokuapp.com/broken_images');
+    cy.get('[src="asdf.jpg"]').then( ($el) => {
+      expect($el[0].naturalWidth).to.be.greaterThan(0);
+      expect($el[0].naturalHeight).to.be.greaterThan(0);
+    });
+  });
+
+  it('Passes if the second image is working', () => {
+    cy.visit('https://the-internet.herokuapp.com/broken_images');
+    cy.get('[src="hjkl.jpg"]').then( ($el) => {
+      expect($el[0].naturalWidth).to.be.greaterThan(0);
+      expect($el[0].naturalHeight).to.be.greaterThan(0);
+    });
+  });
+
+  it('Passes if the third image is working', () => {
+    cy.visit('https://the-internet.herokuapp.com/broken_images');
+    cy.get('[src="img/avatar-blank.jpg"]').then( ($el) => {
+      expect($el[0].naturalWidth).to.be.greaterThan(0);
+      expect($el[0].naturalHeight).to.be.greaterThan(0);
+    });
+  });
+})
