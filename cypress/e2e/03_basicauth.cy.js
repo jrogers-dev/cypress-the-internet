027eb4262355aa12204307c369d97d435b437586
Mimetic <jrogers.dev@gmail.com>
2023-03-01 17:57:12 -0800

    Added spec file for the Basic_Auth page

---
+++ cypress/e2e/03_basicauth.cy.js
@@ -0,0 +1,20 @@
+describe('Completes tests on elements at https://the-internet.herokuapp.com/basic_auth', () => {
+  it('Passes if the page exists, does not attempt to authenticate', () => {
+    cy.visit('https://the-internet.herokuapp.com/basic_auth', {failOnStatusCode: false});
+  });
+
+  it('Passes login by appending the username and password in URL', function () {
+    cy.visit('https://admin:admin@the-internet.herokuapp.com/basic_auth')
+    cy.get('p').should('contain.text', 'Congratulations! You must have the proper credentials.')
+  });
+
+  it('Successfully login using headers', function () {
+    cy.visit("https://the-internet.herokuapp.com/basic_auth", {
+        headers: {
+            authorization: 'Basic YWRtaW46YWRtaW4='
+        },
+        failOnStatusCode: false
+    })
+    cy.get('p').should('include.text', 'Congratulations! You must have the proper credentials.')
+  });
+})
