0474a99dc12095b158bb7c466e3a3f9fed2c426b
Mimetic <jrogers.dev@gmail.com>
2023-03-05 15:24:34 -0800

    Added spec file for the context-menu page

---
+++ cypress/e2e/07_contextmenu.cy.js
@@ -0,0 +1,18 @@
+describe('Completes tests on elements at https://the-internet.herokuapp.com/context_menu', () => {
+  it('Passes if the page exists', () => {
+    cy.visit('https://the-internet.herokuapp.com/context_menu');
+  });
+
+  it('Passes if the clickable hotspot exists', () => {
+    cy.visit('https://the-internet.herokuapp.com/context_menu');
+    cy.get("#hot-spot").should('exist');
+  });
+
+  it('Passes if rightclicking the hotspot fires an alert with the correct text', () => {
+    cy.visit('https://the-internet.herokuapp.com/context_menu');
+    cy.get("#hot-spot").rightclick();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('You selected a context menu');
+    })
+  });
+})
